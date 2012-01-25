a81d1bae94784cd4fcac299d91f557df5d8008c2
Yutong Wang <hybridthesis@gmail.com>
2012-01-25 01:00:49 -0500

    Stuff

--- Main.hs
+++ Main.hs
@@ -1,5 +1,5 @@
-import Graphics.SOE
-main0
+import SOE
+main
    = runGraphics $
      do w <- openWindow "My First Graphics Program" (300, 300)
         drawInWindow w (text (100, 200) "HelloGraphicsWorld")
