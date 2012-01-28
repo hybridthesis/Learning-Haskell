293fd35fced05aef785b0caf576de904c752a420
Yutong Wang <hybridthesis@gmail.com>
2012-01-28 17:17:10 -0500

    Deleted extraneous files

--- Main.hs
+++
@@ -1,7 +0,0 @@
-import SOE
-main
-   = runGraphics $
-     do w <- openWindow "My First Graphics Program" (300, 300)
-        drawInWindow w (text (100, 200) "HelloGraphicsWorld")
-        k <- getKey w
-        closeWindow w
